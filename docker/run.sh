fb8c1a5ef86889e58a179340b176e882123ba2c7
kumaxxp <xxp@ezweb.ne.jp>
2025-08-31 15:28:09 +0900

    JetCamサポートを追加し、OpenCVやGStreamerの依存関係をDockerfileに追加。run.shを修正してデバイスのグループ追加を整理。

--- docker/run.sh
+++ docker/run.sh
@@ -3,17 +3,19 @@ for d in /dev/i2c-* /dev/gpiochip*; do [ -e "$d" ] && ARGS="$ARGS --device $d"; 
 
 docker run --rm -it \
   --privileged \
-  --group-add $(getent group i2c | cut -d: -f3) \
-  --group-add $(getent group gpio | cut -d: -f3) \
   --runtime nvidia --network host --ipc=host \
   --name jetracer-jp62 \
+  $ARGS \
   --device /dev/video0 --device /dev/video1 \
   --device /dev/i2c-1 \
-  --name jetracer-jp62 \
+  -v /tmp/argus_socket:/tmp/argus_socket \
+  --group-add $(getent group i2c | cut -d: -f3) \
+  --group-add $(getent group gpio | cut -d: -f3) \
+  --group-add $(getent group input | cut -d: -f3) \
+  --group-add $(getent group video | cut -d: -f3) \
   -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix \
   -v /home/kuma/Public/fabo/jetracer/notebooks:/workspace/notebooks:rw \
-  -v $PWD/data:/workspace/data \
+  -v "$PWD/data":/workspace/data \
   -p 8888:8888 \
   jetracer62:latest
-  
-  
+
